d5026a5c64e49642cf15474754557da3a2c4ceb1
Economy Lin <economy.lin@gmail.com>
2012-10-13 00:24:09 +0800

    Improve the page for analyse the target page structure
    Parse info from json string

--- src/webservice/handlers.py
+++ src/webservice/handlers.py
@@ -14,68 +14,58 @@ class FetchPage(webapp2.RequestHandler):
 
     def get(self):
         templateValues = {
+            'newssource': {'active': True},
         }
         self._render(templateValues)
 
     def post(self):
-        slug = self.request.get('slug')
-        name = self.request.get('name')
-        url = self.request.get('url')
-        preventcache = not not self.request.get('preventcache')
-        useragent = self.request.get('useragent')
-        proxy = self.request.get('proxy')
-        cookie = self.request.get('cookie')
-        timeout = self.request.get('timeout')
-        if timeout:
-            timeout = int(timeout)
-        encoding = self.request.get('encoding')
+        action = self.request.get('action')
         fetchurl = self.request.get('fetchurl')
-        parsedencoding = self.request.get('parsedencoding')
-
-        content = self.request.get('content')
         selector = self.request.get('selector')
-        items = []
+        if action == 'JSON':
+            jsonstr = self.request.get('jsonstr')
+            newssource = json.loads(jsonstr)
+        else:
+            newssource = {}
+            newssource['active'] = bool(self.request.get('active'))
+            newssource['slug'] = self.request.get('slug')
+            newssource['name'] = self.request.get('name')
+            newssource['fetchurl'] = fetchurl
+            newssource['preventcache'] = bool(self.request.get('preventcache'))
+            newssource['useragent'] = self.request.get('useragent')
+            newssource['proxy'] = self.request.get('proxy')
+            newssource['cookie'] = self.request.get('cookie')
+            timeout = self.request.get('timeout')
+            if timeout:
+                newssource['timeout'] = int(timeout)
+            newssource['encoding'] = self.request.get('encoding')
+            newssource['selector'] = selector
+            jsonstr = json.dumps(newssource)
 
-        if not content and url:
-            fetcher = ContentFetcher(url, preventcache=preventcache,
-                           useragent=useragent, cookie=cookie,
-                           timeout=timeout, encoding=encoding, proxy=proxy
+        if 'active' not in newssource:
+            newssource['active'] = True
+
+        parsedencoding = self.request.get('parsedencoding')
+        parsedurl = self.request.get('parsedurl')
+        content = self.request.get('content')
+        items = []
+        if (not content or not selector) and fetchurl:
+            fetcher = ContentFetcher(fetchurl, preventcache=newssource['preventcache'],
+                           useragent=newssource['useragent'], proxy=newssource['proxy'],
+                           cookie=newssource['cookie'],
+                           timeout=newssource.get('timeout'), encoding=newssource['encoding']
                          )
-            fetchurl, parsedencoding, content = fetcher.fetch()
+            parsedurl, parsedencoding, content = fetcher.fetch()
         if content and selector:
             parser = HtmlContentParser()
-            items = parser.parse(url, selector, content)
-        newscenter = {
-                    'slug': slug,
-                    'name': name,
-                    'fetchurl': url,
-                    'selector': selector,
-        }
-        if preventcache:
-            newscenter['preventcache'] = preventcache
-        if useragent:
-            newscenter['useragent'] = useragent
-        if cookie:
-            newscenter['cookie'] = cookie
-        if timeout:
-            newscenter['timeout'] = int(timeout)
-        if encoding:
-            newscenter['encoding'] = encoding
+            items = parser.parse(selector, selector, content)
         templateValues = {
-            'url': url,
-            'preventcache': preventcache,
-            'useragent': useragent,
-            'cookie': cookie,
-            'timeout': timeout,
-            'encoding': encoding,
-            'proxy': proxy,
+            'newssource': newssource,
             'content': content,
-            'fetchurl': fetchurl,
             'parsedencoding': parsedencoding,
-            'content': content,
-            'selector': selector,
+            'parsedurl': parsedurl,
             'items': items,
-            'newscenter': json.dumps(newscenter),
+            'jsonstr': jsonstr,
         }
         self._render(templateValues)
 
