1a8b3b7f5d42be4e497005fd6149a028930d5ff6
Economy Lin <economy.lin@gmail.com>
2012-10-17 23:09:59 +0800

    Fix bug in fetch page test page
    Data from json may miss some fields.

--- src/webservice/handlers.py
+++ src/webservice/handlers.py
@@ -20,17 +20,18 @@ class FetchPage(webapp2.RequestHandler):
 
     def post(self):
         action = self.request.get('action')
-        fetchurl = self.request.get('fetchurl')
-        selector = self.request.get('selector')
         if action == 'JSON':
             jsonstr = self.request.get('jsonstr')
             newssource = json.loads(jsonstr)
+            parsedencoding = ''
+            parsedurl = ''
+            content = ''
         else:
             newssource = {}
             newssource['active'] = bool(self.request.get('active'))
             newssource['slug'] = self.request.get('slug')
             newssource['name'] = self.request.get('name')
-            newssource['fetchurl'] = fetchurl
+            newssource['fetchurl'] = self.request.get('fetchurl')
             newssource['preventcache'] = bool(self.request.get('preventcache'))
             newssource['useragent'] = self.request.get('useragent')
             newssource['proxy'] = self.request.get('proxy')
@@ -39,26 +40,28 @@ class FetchPage(webapp2.RequestHandler):
             if timeout:
                 newssource['timeout'] = int(timeout)
             newssource['encoding'] = self.request.get('encoding')
-            newssource['selector'] = selector
             jsonstr = json.dumps(newssource)
+            parsedencoding = self.request.get('parsedencoding')
+            parsedurl = self.request.get('parsedurl')
+            if parsedurl and not parsedurl.startswith(newssource.get('fetchurl')):
+                newssource['selector'] = ''
+                content = ''
+            else:
+                newssource['selector'] = self.request.get('selector')
+                content = self.request.get('content')
 
         if 'active' not in newssource:
             newssource['active'] = True
 
-        parsedencoding = self.request.get('parsedencoding')
-        parsedurl = self.request.get('parsedurl')
-        content = self.request.get('content')
         items = []
-
-        if parsedurl and not parsedurl.startswith(fetchurl):
-            newssource['selector'] = ''
-            content = ''
+        selector = newssource.get('selector')
+        fetchurl = newssource.get('fetchurl')
 
         if (not content or not selector) and fetchurl:
-            fetcher = ContentFetcher(fetchurl, preventcache=newssource['preventcache'],
-                           useragent=newssource['useragent'], proxy=newssource['proxy'],
-                           cookie=newssource['cookie'],
-                           timeout=newssource.get('timeout'), encoding=newssource['encoding']
+            fetcher = ContentFetcher(fetchurl, preventcache=newssource.get('preventcache'),
+                           useragent=newssource.get('useragent'), proxy=newssource.get('proxy'),
+                           cookie=newssource.get('cookie'),
+                           timeout=newssource.get('timeout'), encoding=newssource.get('encoding')
                          )
             parsedurl, parsedencoding, content = fetcher.fetch()
         if content and selector:
